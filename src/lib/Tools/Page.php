0bf4b110aa352c7f369827d4004ea15ac238b9ff
Razvan <tangent.jotey@gmail.com>
2022-01-01 15:55:58 +0100

    Create Page.php

---
+++ src/lib/Tools/Page.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CatPaw\MYSQL\Tools;
+
+use JetBrains\PhpStorm\Pure;
+
+class Page {
+	private function __construct(
+		private string $limit
+	) {
+	}
+
+	public function __toString(): string {
+		return $this->limit;
+	}
+
+	#[Pure] public static function of(
+		int $offset, int $length
+	): self {
+		return new Page("limit $offset, $length");
+	}
+
+	#[Pure] public static function length(
+		int $length
+	): self {
+		return new Page("limit $length");
+	}
+}
